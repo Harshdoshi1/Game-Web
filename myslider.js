b222c3d37cb143187d6a71657eceb96fc35a3b97
Harsh Doshi <115213620+Harshdoshi1@users.noreply.github.com>
2024-07-04 21:07:34 +0530

    push

---
+++ myslider.js
@@ -0,0 +1,8 @@
+
+const slide = document.querySelectorAll("slider");
+let slideIndex = 0;
+let interval = null;
+
+const buttonPrev = document.querySelector("prev");
+const buttonNext = document.querySelector("next");
+
